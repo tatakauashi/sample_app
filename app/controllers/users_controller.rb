9475d04790b6d12faff8906873f414abd622df57
vagrant <vagrant@localhost.localdomain>
2015-01-23 16:02:59 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,24 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存が成功した場合
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      # 保存が失敗した場合
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
