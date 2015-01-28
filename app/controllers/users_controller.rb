93d0c8c8d7f8e3e98edb6353da43cbeaa8615c2e
vagrant <vagrant@localhost.localdomain>
2015-01-28 15:26:28 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -20,9 +34,46 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+#    @user = User.find(params[:id])
+  end
+
+  def update
+#    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      p = params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      if p[:password_confirmation].nil?
+        p[:password_confirmation] = p[:password]
+      end
+      return p
+    end
+
+    # Before actions
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
